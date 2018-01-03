88db045e7184e6031a8f7c572df355265101c6c3
Radu Maier <ariademur@webserv.co>
2018-01-03 18:48:20 +0100

    Basic index.php and App class

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Project\App(__DIR__);
+/**
+ * Initialize app.
+ *
+ * Parameter is the web accessible project directory path
+ * (make sure to add the trailing slash).
+ */
+$app = new \Project\App(__DIR__.'/');
+//$app = new \Project\App(null);
 $app->start();
 $app->run();
 $app->stop();
--- src/App.php
+++ src/App.php
@@ -2,9 +2,15 @@
 
 namespace Project;
 
-class App extends \WebServCo\Framework\Libraries\Application
+class App extends \WebServCo\Framework\Application
 {
-    public function __construct()
+    public function __construct($pathPublic, $pathProject = null)
     {
+        /**
+         * Project can be located in a completely different place.
+         */
+        $pathProject = $pathProject ?: realpath(__DIR__ . '/..') . '/';
+        
+        parent::__construct($pathPublic, $pathProject);
     }
 }
