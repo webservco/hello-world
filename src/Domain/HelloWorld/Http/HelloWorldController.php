78d5bfcd1cc612f3ea9870bd028bb535ec42cfa5
Radu Maier <ariademur@webserv.co>
2018-01-07 13:02:04 +0100

    Implement OutputLoader

--- src/Domain/HelloWorld/Http/HelloWorldController.php
+++ src/Domain/HelloWorld/Http/HelloWorldController.php
@@ -5,9 +5,23 @@ use WebServCo\Framework\Framework as Fw;
 
 final class HelloWorldController extends \WebServCo\Framework\AbstractController
 {
+    public function __construct()
+    {
+        parent::__construct(new \Project\OutputLoader);
+    }
+    
     final public function hello($simplified = false)
     {
-        echo $simplified ? 'Hi!' : 'Hello World!';
+        //echo $simplified ? 'Hi!' : 'Hello World!';
+        
+        //optionally set main template
+        
+        $data['title'] = 'Hello World!';
+        
+        //$this->output()->html($data, $template);
+        //Fw::output->html($data, $template);
+        //or
+        echo $this->output()->json($data);
     }
     
     final public function foo()
