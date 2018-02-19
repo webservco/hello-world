2d3c78aba0c397ffed566524e15ca98f336e0c00
Radu Maier <ariademur@webserv.co>
2018-02-19 11:41:02 +0100

    Cleanup ControllerTrait

--- src/Traits/ControllerTrait.php
+++ src/Traits/ControllerTrait.php
@@ -4,8 +4,4 @@ namespace Project\Traits;
 trait ControllerTrait
 {
     use ControllerViewTrait;
-    
-    abstract protected function initI18n();
-    
-    abstract protected function setLanguage($lang);
 }
