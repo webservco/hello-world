940f890c46670224b77646c95ab7443045082c01
Radu Maier <ariademur@webserv.co>
2018-02-25 20:01:26 +0100

    layout cleanup

--- resources/views/blog/postItem.php
+++ resources/views/blog/postItem.php
@@ -1,17 +1,4 @@
-                <section class="post">
-                    <header class="post-header">
-                        <img width="48" height="48" alt="Avatar" class="post-avatar" src="assets/img/tape.png">
-                        <h2 class="post-title"><?=$this->data('title','{TITLE}')?></h2>
-                        <p class="post-meta">
-                            Tags:
-                            <a class="post-category post-category-design" href="#">CSS</a>
-                            <a class="post-category post-category-js" href="#">JavaScript</a>
-                            <a class="post-category" href="#">Uncategorized</a>
-                        </p>
-                    </header>
-                    <div class="post-description">
-                        <p>
-                            <?=$this->data('content','{CONTENT}')?>
-                        </p>
-                    </div>
-                </section>
+            <div class="blog-post">
+                    <h2><?=$this->data('title','{TITLE}')?></h2>
+                    <p><?=$this->data('content','{CONTENT}')?></p>
+            </div>
--- resources/views/blog/posts.php
+++ resources/views/blog/posts.php
@@ -1,4 +1,12 @@
-                <?php
-                foreach($this->data('posts',[]) as $post) {
-                    echo $post['html'];
-                } ?>
+        <div class="container">
+            <div class="content">
+                
+                <h1><?=$this->data('meta/title', __('Posts'))?></h1>
+                
+<?php foreach($this->data('posts',[]) as $post) {
+    echo $post['html'];
+} ?>                
+
+            </div><?php //content ?>
+        </div><?php //container ?>
+
