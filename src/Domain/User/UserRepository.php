c09ddb54db9a8eb5f7a5875a117c8a733c45b7a3
Radu Maier <ariademur@webserv.co>
2018-03-04 12:43:43 +0100

    Cleanup UserRepository

--- src/Domain/User/UserRepository.php
+++ src/Domain/User/UserRepository.php
@@ -3,8 +3,4 @@ namespace Project\Domain\User;
 
 final class UserRepository extends \Project\AbstractRepository
 {
-    public function login($email, $password, $remember)
-    {
-        //XXX
-    }
 }
