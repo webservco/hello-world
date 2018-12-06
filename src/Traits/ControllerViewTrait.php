54b591fd38d68480b732d5cd083dcdd63fae40c0
Radu Maier <ariademur@webserv.co>
2018-12-06 18:34:48 +0100

    Fix "Parameter #1 $str of function strtolower expects string, string|false          given."

--- src/Traits/ControllerViewTrait.php
+++ src/Traits/ControllerViewTrait.php
@@ -5,13 +5,13 @@ trait ControllerViewTrait
 {
     abstract public function data($key, $defaultValue = false);
     abstract protected function setData($key, $value);
-    
+
     protected function initViews($namespace)
     {
         $parts = explode('\\', $namespace);
-        $this->setData('dir/views', strtolower(end($parts)));
+        $this->setData('dir/views', strtolower((string) end($parts)));
     }
-    
+
     protected function getView($templateName)
     {
         return sprintf('%s/%s', $this->data('dir/views'), $templateName);
