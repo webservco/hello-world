f070013c36af836523697ce2818ba30bd97b8809
Radu Maier <ariademur@webserv.co>
2019-11-06 10:06:30 +0100

    fix ctrl

--- src/Traits/ControllerTrait.php
+++ src/Traits/ControllerTrait.php
@@ -7,14 +7,14 @@ trait ControllerTrait
     abstract protected function initDomain();
     abstract protected function initMeta($action);
     abstract protected function setData($key, $value);
-    
-    protected function initPaths()
+
+    protected function setupPaths()
     {
         $this->setData('path', $this->config()->get('app/path'));
         $this->setData('url/app', $this->request()->getAppUrl());
         $this->setData('url/lang', $this->request()->getUrl(['lang']));
     }
-    
+
     /**
      * Called (optionally) by each method.
      */
