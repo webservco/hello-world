d6e25f0fd219f87b3d1431e098b78504927858f6
Radu Maier <ariademur@webserv.co>
2020-09-30 12:23:28 +0200

    dataElse

--- resources/views/user/account.php
+++ resources/views/user/account.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="content">
 
-        <h1><?=sprintf('Hello %s!', $this->when('user/name', 'N/A'))?></h1>
+        <h1><?=sprintf('Hello %s!', $this->dataElse('user/name', 'N/A'))?></h1>
 
         <div class="btn-group" role="group" aria-label="<?=__('Actions')?>">
             <a class="btn btn-light" href="<?=$this->data('url/app','/')?>User/logout" title="User/logout">User/logout</a>
